62a24e499c6ef0a8c57be21fc509c6b94ac12a43
pampew10 <47994490+pampew10@users.noreply.github.com>
2020-11-29 13:20:25 -0700

    Add files via upload

---
+++ Port Verifier.py
@@ -0,0 +1,32 @@
+import os
+
+#Scans most common
+nmap = "nmap 192.168.1.1"
+nmapoutput = os.popen(nmap).read()
+nmap2 = "nmap 192.168.2.1"
+nmapoutput2 = os.popen(nmap).read()
+
+nmapoutputarray = []
+nmapoutputarray2 = []
+unmatcheditems = []
+
+lines = 0
+lines2 = 0
+for line in nmapoutput.splitlines():
+    lines = lines + 1
+    if lines >= 6:
+        print(line)
+        nmapoutputarray.append(line)
+
+for line in nmapoutput2.splitlines():
+    lines2 = lines2 + 1
+    if lines2 >= 6:
+        print(line)
+        nmapoutputarray2.append(line)
+
+for i in nmapoutputarray:
+    if i in nmapoutputarray2:
+        print("No")
+    else:
+        unmatcheditems.append(i)
+print(unmatcheditems)
